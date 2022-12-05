f10389c50aceb01d28da03bea7df6dcd1e629e76
Niels Rymenams <niels@zabun.be>
2022-12-05 14:03:26 +0100

    Add basic SfHierarchyGridDemo

---
+++ SfPlayground/Models/Property.cs
@@ -0,0 +1,56 @@
+﻿using System.Text.Json.Serialization;
+using SfPlayground.Helpers;
+
+namespace SfPlayground.Models;
+
+public class Property
+{
+    public long PropertyAutoId { get; set; }
+    public int CompanyId { get; set; }
+    public string? CompanyLanguage { get; set; }
+    public string? Reference { get; set; }
+    public string? Transaction { get; set; }
+    public int TypeId { get; set; }
+    public string? Type { get; set; }
+    public string? Availability { get; set; }
+    public string? ConstructionYear { get; set; }
+    public string? BuildingType { get; set; }
+    public string? Street { get; set; }
+    public string? Number { get; set; }
+    public string? Box { get; set; }
+    public string? FileAdministrator { get; set; }
+    public string? Office { get; set; }
+    public string? Owner { get; set; }
+    public string? Renter { get; set; }
+    public string? Buyer { get; set; }
+
+    [JsonConverter(typeof(BitBoolJsonConverter))]
+    public bool Development { get; set; }
+
+    [JsonConverter(typeof(BitBoolJsonConverter))]
+    public bool Investment { get; set; }
+
+    [JsonConverter(typeof(BitBoolJsonConverter))]
+    public bool Poster { get; set; }
+
+    public int? Bedrooms { get; set; }
+    public string? Key { get; set; }
+    public string? Status { get; set; }
+    public string? ResponsibleSalesRep { get; set; }
+
+    [JsonConverter(typeof(BitBoolJsonConverter))]
+    public bool Archived { get; set; }
+
+    [JsonConverter(typeof(BitBoolJsonConverter))]
+    public bool Show { get; set; }
+
+    public int Flag { get; set; }
+    public long? ProjectPropertyAutoId { get; set; }
+    public string? ProjectReference { get; set; }
+
+    [JsonConverter(typeof(DateTimeJsonConverter))]
+    public DateTime? Creation { get; set; }
+
+    [JsonConverter(typeof(DateTimeJsonConverter))]
+    public DateTime? Modified { get; set; }
+}
---
+++ SfPlayground/Pages/SfHierarchyGridDemo.razor.cs
@@ -0,0 +1,74 @@
+﻿using System.Net.Http.Json;
+using Microsoft.AspNetCore.Components;
+using SfPlayground.Models;
+using Syncfusion.Blazor.Grids;
+
+namespace SfPlayground.Pages;
+
+public partial class SfHierarchyGridDemo
+{
+    [Inject] private HttpClient? Http { get; set; }
+
+    private List<Property> _mainProperties = new();
+    private List<Property> _properties = new();
+    private SfGrid<Property>? _grid;
+    private string? _savedState;
+
+    protected override async Task OnInitializedAsync()
+    {
+        _properties = await Http!.GetFromJsonAsync<List<Property>>("sample-data/properties.json") ?? new List<Property>();
+        _mainProperties = _properties.Where(p => p.ProjectPropertyAutoId == null).ToList();
+
+        base.OnInitialized();
+    }
+
+    private List<Property> GetProjectProperties(long projectPropertyAutoId)
+    {
+        return _properties.Where(p => p.ProjectPropertyAutoId == projectPropertyAutoId).ToList();
+    }
+
+    private async Task OnLoadStateClicked()
+    {
+        if (_grid != null && !string.IsNullOrEmpty(_savedState))
+        {
+            await _grid.SetPersistDataAsync(_savedState);
+            StateHasChanged();
+        }
+    }
+
+    private async Task OnLoadStateFromJsonClicked()
+    {
+        if (_grid != null)
+        {
+            var gridState = await Http!.GetStringAsync("sample-data/gridstate.json") ?? null;
+
+            await _grid.SetPersistDataAsync(gridState);
+            StateHasChanged();
+        }
+    }
+
+    private async Task OnResetClickedAsync()
+    {
+        if (_grid != null)
+        {
+            await _grid.ResetPersistDataAsync();
+            StateHasChanged();
+        }
+    }
+
+    private void OnRowDataBound(RowDataBoundEventArgs<Property> args)
+    {
+        if (args.Data?.TypeId != 21 || !_properties.Any(p => p.ProjectPropertyAutoId == args.Data.PropertyAutoId))
+        {
+            args.Row.AddClass(new string[] { "e-detail-disable" });
+        }
+    }
+
+    private async Task OnSaveStateClickedAsync()
+    {
+        if (_grid != null)
+        {
+            _savedState = await _grid.GetPersistDataAsync();
+        }
+    }
+}
